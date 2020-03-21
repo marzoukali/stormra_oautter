7d4a85babbda06cb817eb03892a5ab4e0436a17d
ahmedmehanna <dev.ahmedmhana@gmail.com>
2020-03-21 17:37:12 +0000

    Update unit test

--- test/user_account_client_test.dart
+++ test/user_account_client_test.dart
@@ -3,21 +3,23 @@ import 'package:stormra_oautter/clients/user_account_client.dart';
 import 'package:stormra_oautter/models/user_registeration_request.dart';
 
 void main() {
-
   test('test register a new user', () async {
-    
-   final client = UserAccountClient('qa-mda-idsrv-app.azurewebsites.net');
-   var userRegisterationInfo = UserRegisterationRequest('email1@test.com','linux.2000', 'linux.2000', 'ahmed', 'ali', 'ali5', '1990-03-17T21:50:43.001Z', 'egypt');
-   var r = await client.register(userRegisterationInfo);
+    final client = UserAccountClient('qa-mda-idsrv-app.azurewebsites.net');
+    var userRegisterationInfo = UserRegisterationRequest(
+        email: 'email1@test.com',
+        password: 'linux.2000',
+        confirmPassword: 'linux.2000',
+        firstName: 'ahmed',
+        lastName: 'ali',
+        userName: 'ali5',
+        birthDate: '1990-03-17T21:50:43.001Z',
+        country: 'Egypt');
+    var r = await client.register(userRegisterationInfo);
 
-   if(r.errorDescription != null)
-   {
-   print(r.errorDescription);
-   }else{
-     print(r.email);
-   }
-
+    if (r.errorDescription != null) {
+      print(r.errorDescription);
+    } else {
+      print(r.email);
+    }
   });
-
-
 }
